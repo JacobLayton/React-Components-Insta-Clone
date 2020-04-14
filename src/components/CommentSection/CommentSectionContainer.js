ca48271abf58d78cf8d42adbf114b3bc1079f07f
JacobLayton <jlayjlay3@gmail.com>
2020-04-13 20:16:25 -0700

    Comments are now displayed, cleaned up useState to fix warnings

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -4,12 +4,16 @@ import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
 
-const CommentSection = props => {
+const CommentSection = (props) => {
   // Add state for the comments
+  const [comments] = useState(props.comments);
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {comments.map((comment) => (
+        <Comment key={comment.text} comment={comment} />
+      ))}
       <CommentInput />
     </div>
   );
--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -9,7 +9,7 @@ import "./Posts.css";
 // pass props in this file to
 const Post = (props) => {
   // set up state for the likes
-  const [postData, setPostData] = useState(props);
+  const [postData] = useState(props);
 
   return (
     <div className="post-border">
